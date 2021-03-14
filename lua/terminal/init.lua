df0bc8763064493eb064002749454494147ccf4a
Morten Paulsen <morten.p96@gmail.com>
2021-03-14 02:56:10 +0100

    Add autocommands

--- lua/terminal/init.lua
+++ lua/terminal/init.lua
@@ -1,21 +1,42 @@
 local config = require('terminal.config')
+local state = require('terminal.state')
+local terminal_factory = require('terminal.internal')
 
 local M = {}
 
+local function create_autocommands()
+  vim.cmd([[augroup Terminal.nvim]])
+  vim.cmd([[  autocmd!]])
+  vim.cmd([[  autocmd TermOpen * call luaeval("require('terminal.autocommand').term_open(_A)", expand('<abuf>'))]])
+  vim.cmd([[  autocmd TermClose * call luaeval("require('terminal.autocommand').term_close(_A)", expand('<abuf>'))]])
+  vim.cmd([[augroup END]])
+end
+
 function M.setup(opts)
+  opts = opts or {}
   -- Update the configuration
   for key, value in pairs(opts) do
     config.set(key, value)
   end
 
-  if config.get('define_all_commands') then
-    vim.cmd([=[command! -nargs=1 TermOpen call luaeval('require("terminal.api").open(_A)', [<f-args>]]=])
-    vim.cmd([=[command! -nargs=1 TermClose call luaeval('require("terminal.api").close(_A)', [<f-args>]]=])
-    vim.cmd([=[command! -nargs=1 TermMove call luaeval('require("terminal.api").move(_A)', [<f-args>]]=])
-    vim.cmd([=[command! -nargs=1 TermEcho call luaeval('require("terminal.api").echo(_A)', [<f-args>]]=])
+  state.insert_terminal(terminal_factory:new(config.get('default_terminal')))
+  -- TODO (mortepau): Fix so that all the terminals have all keys
+  for _, terminal_config in ipairs(opts.terminals or {}) do
+    state.insert_terminal(terminal_factory:new(terminal_config))
   end
 
-  vim.cmd([=[command! -nargs=1 Terminal call luaeval('require("terminal.api").execute(_A)', [<f-args>]]=])
+  if config.get('define_all_commands') then
+    vim.cmd([=[command! -nargs=? TermOpen call luaeval('require("terminal.api").open(_A)', <f-args>)]=])
+    vim.cmd([=[command! -nargs=? TermClose call luaeval('require("terminal.api").close(_A)', <f-args>)]=])
+    vim.cmd([=[command! -nargs=? TermMove call luaeval('require("terminal.api").move(_A)', <f-args>)]=])
+    vim.cmd([=[command! -nargs=? TermEcho call luaeval('require("terminal.api").echo(_A)', <f-args>)]=])
+  end
+
+  -- TODO (mortepau): Add completelist for external commands
+  vim.cmd([=[command! -nargs=? Terminal call luaeval('require("terminal.api").execute(_A)', <f-args>)]=])
+  vim.cmd([=[command! -nargs=0 -bang TermList call luaeval('require("terminal.api").list(_A == 1)', <bang>0)]=])
+
+  create_autocommands()
 end
 
 return M
